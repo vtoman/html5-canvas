386e10c36f7f43ec9970338ed4cec71142cda8d6
vtoman <vtoman@gmail.com>
2023-01-18 19:25:44 +0100

    Canvas handling now only after page load.

--- 1-hello-world/canvas-script.js
+++ 1-hello-world/canvas-script.js
@@ -1,11 +1,15 @@
-/** @type {HTMLCanvasElement} */
+ /** @type {HTMLCanvasElement} */
 
-var canvas = document.getElementById("hello-world-canvas");
-var context = canvas.getContext("2d");
+window.onload = function() { 
+  var canvas = document.getElementById("hello-world-canvas");
+  var context = canvas.getContext("2d");
 
-context.fillStyle = "blue";
-context.fillRect(10, 40, 30, 70);
+  context.fillStyle = "blue";
+  context.fillRect(10, 40, 30, 70);
 
-context.fillStyle = "yellow";
-context.fillRect(50, 30, 60, 30);
+  context.fillStyle = "yellow";
+  context.fillRect(50, 30, 60, 30);
+}
+
+
 
